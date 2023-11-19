3d134393791a360dd1f682e18ad1d90d89869da9
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2023-11-19 00:00:36 -0600

    Create function Start-TcpListener.ps1

---
+++ VideoNotes/function Start-TcpListener.ps1
@@ -0,0 +1,41 @@
+function Start-TcpListener {
+    param(
+        [int]$Port = 2222,
+        [string]$LogFile = "root\logs\log.txt"
+    )
+
+    # Ensure the logs directory exists
+    $logDir = Split-Path -Path $LogFile -Parent
+    if (-not (Test-Path -Path $logDir)) {
+        New-Item -ItemType Directory -Path $logDir
+    }
+
+    # Loop to keep the listener running
+    while ($true) {
+        # Create a TCP Listener
+        $listener = New-Object System.Net.Sockets.TcpListener '0.0.0.0', $Port
+        $listener.Start()
+
+        try {
+            # Wait for a client connection
+            $client = $listener.AcceptTcpClient()
+
+            # Get the network stream
+            $stream = $client.GetStream()
+            $reader = New-Object System.IO.StreamReader($stream)
+
+            # Read data from the client
+            $data = $reader.ReadToEnd()
+
+            # Append data to the log file
+            Add-Content -Path $LogFile -Value $data
+
+            # Clean up
+            $reader.Close()
+            $client.Close()
+        }
+        finally {
+            $listener.Stop()
+        }
+    }
+}
