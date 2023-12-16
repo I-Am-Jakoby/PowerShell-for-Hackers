7192eccf8ed19644dab3f69f2851b249cccf6023
I-Am-Jakoby <Michaelryanchitwood@gmail.com>
2023-12-16 14:44:31 -0600

    Update 123.ps1

--- Functions/123.ps1
+++ Functions/123.ps1
@@ -1,2 +1,8 @@
-write-host "this is a test" -foregroundcolor 'green'
-Start-Process Calc
+Add-Type -AssemblyName System.Windows.Forms
+$o=New-Object -ComObject WScript.Shell
+$url = -join($channel,"?sub_confirmation=1")
+Start-Process "$url" 
+Start-Sleep -Seconds 5
+[System.Windows.Forms.SendKeys]::SendWait('{TAB}'*2)
+[System.Windows.Forms.SendKeys]::SendWait('{ENTER}')
+Start-Sleep -Seconds 1
